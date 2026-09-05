9ec69a6a32901b8c9005a968db434ef399e3f376
OM252005 <ocproff@gmail.com>
2026-09-05 11:26:48 +0530

    Update gpio.h

--- gpio.h
+++ gpio.h
@@ -1,165 +1,115 @@
-#ifndef GPIO_H
-#define GPIO_H
-#include <stdint.h>
-
-typedef struct {
-    volatile uint32_t MODER;      //0
-    volatile uint32_t OTYPER;        
-    volatile uint32_t OSPEEDR;
-    volatile uint32_t PUPDR;
-    volatile uint32_t IDR;
-    volatile uint32_t ODR;
-    volatile uint32_t BSRR;   
-    volatile uint32_t LCKR;
-    volatile uint32_t AFRL;
-    volatile uint32_t AFRH;  // 024
-     
-
-}GPIO_TypeDef;
+#ifndef GPIO_H// providing gaurd band
+#define  GPIO_H// defining the gaurd band
+#include<stdint.h>
 
 
-
-
-#define GPIOA_BASE 0x48000000UL
-#define GPIOB_BASE 0x48000400UL
-#define GPIOC_BASE 0x48000800UL
-#define GPIOD_BASE 0x48000C00UL
-#define GPIOE_BASE 0x48001000UL
-#define GPIOF_BASE 0x48001400UL
-
-
-
-
-// defining the marcos 
-
-#define GPIOA ((GPIO_TypeDef *)GPIOA_BASE) 
-#define GPIOB ((GPIO_TypeDef *)GPIOB_BASE)
-#define GPIOC ((GPIO_TypeDef *)GPIOC_BASE)
-#define GPIOD ((GPIO_TypeDef *)GPIOD_BASE)
-#define GPIOE ((GPIO_TypeDef *)GPIOE_BASE)
-#define GPIOF ((GPIO_TypeDef *)GPIOF_BASE)
-
-
-
-
-
-
-
-typedef enum // REMEMBER THET ENUM ALWAY DEFINE THE NUMBER IN SEQUENCED AND PLS DO'NOT MISS TYH NUMBER CONFIG ACCORDING TO THE MAIN FUNCTION 
- // 
-{
-    GPIO_PIN_0 = 0,
-    GPIO_PIN_1,
-    GPIO_PIN_2,
-    GPIO_PIN_3,
-    GPIO_PIN_4,
-    GPIO_PIN_5,
-    GPIO_PIN_6,
-    GPIO_PIN_7,
-    GPIO_PIN_8,
-    GPIO_PIN_9,
-    GPIO_PIN_10,
-    GPIO_PIN_11,
-    GPIO_PIN_12,
-    GPIO_PIN_13,
-    GPIO_PIN_14,
-    GPIO_PIN_15
-
-} GPIO_Pin_t;
-
-
-
-typedef enum{// moder gpio
-    GPIO_MODE_INPUT = 0,
-    GPIO_MODE_OUTPUT,
-    GPIO_MODE_AF,
-    GPIO_MODE_ANALOG
-
-}GPIO_Mode_t;
-
-
-
-
-typedef enum{  //otyper gpio
-    GPIO_OTYPER_PP = 0,
-    GPIO_OTYPER_OD
-
-}GPIO_OutputType_t;
-
-
-typedef enum //speed gpio
-{
-    GPIO_SPEEDR_LOW = 0,
-    GPIO_SPEEDR_MEDIUM,
-    GPIO_SPEEDR_HIGH, 
-    GPIO_SPEEDR_VERY_HIGH
-
-}GPIO_Speed_t;
-
-
-typedef enum // pull up and down gpio
-{
-    GPIO_NOPULL = 0,
-    GPIO_PULLUP,
-    GPIO_PULLDOWN
-
-} GPIO_Pull_t;
-
-
-typedef enum // alternate function 
-{
-    GPIO_AF0 = 0,
-    GPIO_AF1,
-    GPIO_AF2,
-    GPIO_AF3,
-    GPIO_AF4,
-    GPIO_AF5,
-    GPIO_AF6,
-    GPIO_AF7,
-    GPIO_AF8,
-    GPIO_AF9,
-    GPIO_AF10,
-    GPIO_AF11,
-    GPIO_AF12,
-    GPIO_AF13,
-    GPIO_AF14,
-    GPIO_AF15
-
-} GPIO_AF_t;
-
-typedef struct// pin configrations 
-{
-    GPIO_Pin_t        Pin;
-    GPIO_Mode_t       Mode;
-    GPIO_OutputType_t OutputType;
-    GPIO_Speed_t      Speed;
-    GPIO_Pull_t       Pull;
-    GPIO_AF_t         Alternate;
-
-} GPIO_PinConfig_t;
+// typedf  enumaration  of gpio ports 
 
 typedef enum
 {
-    GPIO_PIN_RESET = 0,
-    GPIO_PIN_SET
-
-} GPIO_PinState_t;
-
-
-
-void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_PinConfig_t *Config);
-
-void GPIO_DeInit(GPIO_TypeDef *GPIOx);
-
-void GPIO_WritePin(GPIO_TypeDef *GPIOx, uint8_t Pin, uint8_t State);
-
-void GPIO_TogglePin(GPIO_TypeDef *GPIOx, uint8_t Pin);
-
-uint8_t GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint8_t Pin);
-
-void GPIO_WritePort(GPIO_TypeDef *GPIOx, uint16_t Value);
-
-uint16_t GPIO_ReadPort(GPIO_TypeDef *GPIOx);
+    GPIO_PORT_A = 0,
+    GPIO_PORT_B,
+    GPIO_PORT_C
+}GPIO_Port;
 
 
-#endif
+
+// GPIO_Pinn NUMBERS 
+typedef enum
+{
+GPIO_PIN_0 = 0,
+GPIO_PIN_1,
+GPIO_PIN_2,
+GPIO_PIN_3,
+GPIO_PIN_4,
+GPIO_PIN_5,
+GPIO_PIN_6,
+GPIO_PIN_7,
+GPIO_PIN_8,
+GPIO_PIN_9,
+GPIO_PIN_10,
+GPIO_PIN_11,
+GPIO_PIN_12,
+GPIO_PIN_13,
+GPIO_PIN_14,
+GPIO_PIN_15,
+
+
+}GPIO_Pin;
+
+
+// DEFINING REGISTER STRUCTURE 
+
+//  each one has 4 bit of data and hexadecimal values 
+
+
+typedef struct
+{
+    volatile uint32_t CRL;      // 0x00
+    volatile uint32_t CRH;      // 0x04
+    volatile uint32_t IDR;      // 0x08
+    volatile uint32_t ODR;      // 0x0C
+    volatile uint32_t BSRR;     // 0x10
+    volatile uint32_t BRR;      // 0x14
+    volatile uint32_t LCKR;     // 0x18
+
+} GPIO_TypeDef;
+
+// PORT BASE ADDRESS 
+
+#define GPIO_PORT_A_BASE 0x40010800UL
+#define GPIO_PORT_B_BASE 0x40010C00UL
+#define GPIO_PORT_C_BASE 0x40011000UL
+
+
+
+
+// PORT BASE BASE ADDRES CONFIGRAT9INO WITNH PYPEDEF PINS 
+#define GPIOA ((GPIO_TypeDef*) GPIO_PORT_A_BASE)
+#define GPIOB ((GPIO_TypeDef*) GPIO_PORT_B_BASE)
+#define GPIOC ((GPIO_TypeDef*) GPIO_PORT_C_BASE)
+
+
+
+//  mode config 
+#define GPIO_MODE_INPUT          0x0U
+#define GPIO_MODE_OUTPUT_10MHZ   0x1U
+#define GPIO_MODE_OUTPUT_2MHZ    0x2U
+#define GPIO_MODE_OUTPUT_50MHZ   0x3U
+
+
+// input config congig
+#define GPIO_INPUT_ANALOG        0x0U
+#define GPIO_INPUT_FLOATING      0x1U
+#define GPIO_INPUT_PULL          0x2U
+
+// 
+#define GPIO_OUTPUT_PUSH_PULL    0x0U
+#define GPIO_OUTPUT_OPEN_DRAIN   0x1U
+#define GPIO_OUTPUT_AF_PP        0x2U
+#define GPIO_OUTPUT_AF_OD        0x3U
+
+// state 
+typedef enum
+{
+    GPIO_STATE_LOW = 0,
+    GPIO_STATE_HIGH = 1
+} GPIO_State;
+
+
+void GPIO_Init(GPIO_TypeDef *GPIOx,
+                   GPIO_Pin pin,
+                   uint32_t mode,
+                   uint32_t cnf);
+
+void GPIO_Write(GPIO_TypeDef *GPIOx,
+                GPIO_Pin pin,
+                GPIO_State state);
+
+
+GPIO_State GPIO_Read(GPIO_TypeDef *GPIOx,
+                     GPIO_Pin pin);
+
+
+#endif// ending guard band 
+
